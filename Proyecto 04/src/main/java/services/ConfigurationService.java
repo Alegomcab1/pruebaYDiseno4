b0d4eac194ccd539c0b477ae972268649ff42147
Alegomcab1 <alegomcab1@gmail.com>
2018-11-26 18:38:51 +0100

    Actualizacion sponsor, section, sponsorShip, Populate y terminados requisitos RANGO A de parte de Juan Carlos y Alejandro

--- Proyecto 04/src/main/java/services/ConfigurationService.java
+++ Proyecto 04/src/main/java/services/ConfigurationService.java
@@ -1,14 +1,19 @@
 package services;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import repositories.ConfigurationRepository;
 import domain.Actor;
 import domain.Application;
 import domain.Box;
+import domain.Configuration;
 import domain.Customer;
 import domain.EducationRecord;
 import domain.Endorser;
@@ -27,6 +32,8 @@ import domain.Section;
 import domain.SocialProfile;
 import domain.Tutorial;
 
+@Service
+@Transactional
 public class ConfigurationService {
 
     @Autowired
@@ -245,5 +252,122 @@ public class ConfigurationService {
 			}
 	    }
 	return result;
+    }
+
+    public String showGoodWords() {
+	return this.configurationRepository.goodWords();
+    }
+
+    public String showBadWords() {
+	return this.configurationRepository.badWords();
+    }
+
+    public String addGoodWords(String word) {
+	Configuration configuration = this.configurationRepository
+		.configuration();
+	String goodWords = configuration.getGoodWords();
+	configuration.setGoodWords(goodWords = goodWords + "," + word);
+	this.configurationRepository.save(configuration);
+
+	return configuration.getGoodWords();
+    }
+
+    public String addBadWords(String word) {
+	Configuration configuration = this.configurationRepository
+		.configuration();
+	String badWords = configuration.getBadWords();
+	configuration.setBadWords(badWords = badWords + "," + word);
+	this.configurationRepository.save(configuration);
+
+	return configuration.getBadWords();
+    }
+
+    public void deleteGoodWord(String word) {
+	String goodWords = this.showGoodWords();
+	Configuration configuration = this.configurationRepository
+		.configuration();
+	List<String> goodWordsList = Arrays.asList(goodWords.split(",[ ]*"));
+
+	for (String g : goodWordsList)
+	    if (g.equals(word))
+		goodWordsList.remove(g);
+
+	String result = "";
+
+	for (int i = 0; i < goodWordsList.size(); i++)
+	    if (i < goodWordsList.size() - 1)
+		result = result + goodWordsList.get(i) + ",";
+	    else
+		result = result + goodWordsList.get(i);
+
+	configuration.setGoodWords(result);
+	this.configurationRepository.save(configuration);
+    }
+
+    public void deleteBadWord(String word) {
+	String badWords = this.showGoodWords();
+	Configuration configuration = this.configurationRepository
+		.configuration();
+	List<String> badWordsList = Arrays.asList(badWords.split(",[ ]*"));
+
+	for (String g : badWordsList)
+	    if (g.equals(word))
+		badWordsList.remove(g);
+
+	String result = "";
+
+	for (int i = 0; i < badWordsList.size(); i++)
+	    if (i < badWordsList.size() - 1)
+		result = result + badWordsList.get(i) + ",";
+	    else
+		result = result + badWordsList.get(i);
+
+	configuration.setBadWords(result);
+	this.configurationRepository.save(configuration);
+    }
+
+    /*
+     * Launch a process that computes a score for every customer and handy
+     * worker. The score is computed building on the endorsements that they’ve
+     * got. The system must analyse the comments in the endorsements and compute
+     * the number of positive words (p) and the number of negative words (n).
+     * The score must be computed as p – n normalised to range -1.00 up to +1.00
+     * using a linear homothetic transformation.
+     */
+
+    public Double computeScore(Endorser e) {
+	String goodWords = this.configurationRepository.goodWords();
+	String badWoords = this.configurationRepository.badWords();
+	Double countGood = 0.0;
+	Double countBad = 0.0;
+	Double total = 0.0;
+	List<Double> parcialresult = new ArrayList<Double>();
+
+	List<String> goodWordsList = Arrays.asList(goodWords.split(",[ ]*"));
+	List<String> badWordsList = Arrays.asList(badWoords.split(",[ ]*"));
+
+	List<Endorsment> endorsments = e.getEndorsments();
+
+	for (Endorsment endo : endorsments)
+	    for (String g : endo.getComments()) {
+		List<String> commentSplit = Arrays.asList(g.split("\\W"));
+		for (String word : commentSplit) {
+		    if (goodWordsList.contains(word))
+			countGood = countGood + 1.0;
+		    if (badWordsList.contains(word))
+			countBad = countBad - 1.0;
+		    total = countGood - countBad;
+		    parcialresult.add((countGood / total) + (countBad / total));
+		}
+
+	    }
+	Double res = 0.0;
+	Double cont = 0.0;
+
+	for (Double d : parcialresult)
+	    cont = cont + d;
+	res = cont / parcialresult.size();
+
+	return res;
     }
 }
